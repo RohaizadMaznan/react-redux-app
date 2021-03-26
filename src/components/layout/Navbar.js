3619613b9785f4f41a704a55b1ffc87c1b40942a
Rohaizad Maznan <rohaizadmaznan@gmail.com>
2021-03-26 14:26:05 +0800

    final commit 2

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,17 +2,30 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import SignedInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedOutLinks'
+import {connect} from 'react-redux'
 
-export default function Navbar() {
+const Navbar = ({auth}) => {
+    //const { auth } = props
+    //console.log(auth)
+
+    const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />
+
     return (
         <div>
             <nav className="nav-wrapper grey darken-3">
                 <div className="container">
                     <Link to='/' className="brand-logo">MarioPlan</Link>
-                    <SignedInLinks />
-                    <SignedOutLinks />
+                    { links }
                 </div>
             </nav>
         </div>
     )
 }
+
+const mapStateToProps = (state) => {
+    return {
+        auth: state.firebase.auth
+    }
+}
+
+export default connect(mapStateToProps)(Navbar)
